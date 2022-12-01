187afcc7e4745314692fd65e1230ba176b66db70
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-12-01 23:49:27 +0600

    Add files via upload

--- DZ3_Nurdin_20-02/main.js
+++ DZ3_Nurdin_20-02/main.js
@@ -10,3 +10,26 @@ for (num; num < 100; num++) {
     console.log(num);
   }
 }
+
+const bank = {
+  nurberCard: "99685223652254",
+  dateEnd: "2025.27.02",
+  nameUser: "Nurdin",
+  typeOfCARD: "Банковская карта",
+  CVC: 173,
+};
+console.log(bank);
+console.log(bank.nameUser);
+const bankInfo = {
+  num: 54,
+  nameBank: "Optima",
+  schedule: "Monday-Friday",
+  nowWorks: true,
+  bankPlace: {
+    city: "Bishek",
+    street: "Manas",
+    homeNum: "119",
+  },
+};
+console.log(bankInfo);
+console.log(bankInfo.bankPlace);
